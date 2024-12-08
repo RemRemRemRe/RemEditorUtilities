3074d4812e46c34b65b1cb77459b7f78c5cc257f
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2024-12-08 14:05:57 +0800

    Fixed: empty class view when DisallowedClasses is empty

--- Source/RemEditorUtilities/Private/ClassFilter/RemEditorUtilitiesClassFilter.cpp
+++ Source/RemEditorUtilities/Private/ClassFilter/RemEditorUtilitiesClassFilter.cpp
@@ -6,13 +6,13 @@
 bool FRemEditorUtilitiesClassFilter::IsClassAllowed(const FClassViewerInitializationOptions& InInitOptions, const UClass* InClass,
 	TSharedRef< FClassViewerFilterFuncs > InFilterFuncs)
 {
-	return !InClass->HasAnyClassFlags(DisallowedClassFlags) && InFilterFuncs->IfInChildOfClassesSet(DisallowedClasses , InClass) == EFilterReturn::Failed
+	return !InClass->HasAnyClassFlags(DisallowedClassFlags) && InFilterFuncs->IfInChildOfClassesSet(DisallowedClasses , InClass) != EFilterReturn::Passed
 		&& InFilterFuncs->IfInChildOfClassesSet(AllowedClasses, InClass) != EFilterReturn::Failed;
 }
 
 bool FRemEditorUtilitiesClassFilter::IsUnloadedClassAllowed(const FClassViewerInitializationOptions& InInitOptions,
 	const TSharedRef< const IUnloadedBlueprintData > InUnloadedClassData, TSharedRef< FClassViewerFilterFuncs > InFilterFuncs)
 {
-	return !InUnloadedClassData->HasAnyClassFlags(DisallowedClassFlags) && InFilterFuncs->IfInChildOfClassesSet(DisallowedClasses , InUnloadedClassData) == EFilterReturn::Failed
+	return !InUnloadedClassData->HasAnyClassFlags(DisallowedClassFlags) && InFilterFuncs->IfInChildOfClassesSet(DisallowedClasses , InUnloadedClassData) != EFilterReturn::Passed
 		&& InFilterFuncs->IfInChildOfClassesSet(AllowedClasses, InUnloadedClassData) != EFilterReturn::Failed;
 }
