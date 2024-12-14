bdb2fcf8a1b8d6cf6ef09bdf1aa8c699c1ac42e1
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2024-12-14 23:28:14 +0800

    Misc: using UnrealBuildTool

--- Source/RemCommonEditor/RemCommonEditor.Build.cs
+++ Source/RemCommonEditor/RemCommonEditor.Build.cs
@@ -1,38 +1,37 @@
-﻿// Copyright RemRemRemRe, All Rights Reserved.
+﻿// Copyright RemRemRemRe. All Rights Reserved.
 
-namespace UnrealBuildTool.Rules
+using UnrealBuildTool;
+
+public class RemCommonEditor : ModuleRules
 {
-	public class RemCommonEditor : ModuleRules
+	public RemCommonEditor(ReadOnlyTargetRules target) : base(target)
 	{
-		public RemCommonEditor(ReadOnlyTargetRules target) : base(target)
-		{
-			ShadowVariableWarningLevel = WarningLevel.Error;
-			IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
-			DefaultBuildSettings = BuildSettingsVersion.Latest;
-			CppStandard = CppStandardVersion.EngineDefault;
-			UnsafeTypeCastWarningLevel = WarningLevel.Warning;
-			
-			bEnableNonInlinedGenCppWarnings = true;
-			bUseUnity = false;
-			
-			PrivateDependencyModuleNames.AddRange(
-				new[]
-				{
-					"Core",
-					"CoreUObject",
-					"Engine",
-					
-					"DeveloperSettings",
-					"PropertyEditor",
-					"GameplayTags",
-					"Slate",
-					"SlateCore",
-					"InputCore",
-
-					"RemCommon",
-					"RemEditorUtilities",
-				}
-			);
-		}
+		ShadowVariableWarningLevel = WarningLevel.Error;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		CppStandard = CppStandardVersion.EngineDefault;
+		UnsafeTypeCastWarningLevel = WarningLevel.Warning;
+		
+		bEnableNonInlinedGenCppWarnings = true;
+		bUseUnity = false;
+		
+		PrivateDependencyModuleNames.AddRange(
+			new[]
+			{
+				"Core",
+				"CoreUObject",
+				"Engine",
+				
+				"DeveloperSettings",
+				"PropertyEditor",
+				"GameplayTags",
+				"Slate",
+				"SlateCore",
+				"InputCore",
+				
+				"RemCommon",
+				"RemEditorUtilities",
+			}
+		);
 	}
 }
--- Source/RemEditorUtilities/RemEditorUtilities.Build.cs
+++ Source/RemEditorUtilities/RemEditorUtilities.Build.cs
@@ -1,37 +1,35 @@
-// Copyright RemRemRemRe, All Rights Reserved.
+// Copyright RemRemRemRe. All Rights Reserved.
 
+using UnrealBuildTool;
 
-namespace UnrealBuildTool.Rules
+public class RemEditorUtilities : ModuleRules
 {
-	public class RemEditorUtilities : ModuleRules
+	public RemEditorUtilities(ReadOnlyTargetRules target) : base(target)
 	{
-		public RemEditorUtilities(ReadOnlyTargetRules target) : base(target)
-		{
-			ShadowVariableWarningLevel = WarningLevel.Error;
-			IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
-			DefaultBuildSettings = BuildSettingsVersion.Latest;
-			CppStandard = CppStandardVersion.EngineDefault;
-			UnsafeTypeCastWarningLevel = WarningLevel.Warning;
-
-			bEnableNonInlinedGenCppWarnings = true;
-			bUseUnity = false;
-
-			PrivateDependencyModuleNames.AddRange(
-				new[]
-				{
-					"Core",
-					"CoreUObject",
-					"Engine",
-					
-					"Slate",
-					"SlateCore",
-					"UnrealEd",
-					"UMG",
-					"ClassViewer",
-
-					"RemCommon",
-				}
-			);
-		}
+		ShadowVariableWarningLevel = WarningLevel.Error;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		CppStandard = CppStandardVersion.EngineDefault;
+		UnsafeTypeCastWarningLevel = WarningLevel.Warning;
+		
+		bEnableNonInlinedGenCppWarnings = true;
+		bUseUnity = false;
+		
+		PrivateDependencyModuleNames.AddRange(
+			new[]
+			{
+				"Core",
+				"CoreUObject",
+				"Engine",
+				
+				"Slate",
+				"SlateCore",
+				"UnrealEd",
+				"UMG",
+				"ClassViewer",
+				
+				"RemCommon",
+			}
+		);
 	}
 }
